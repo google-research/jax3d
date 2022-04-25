f08ae174c80cd6c597f8fe5417168c82857eb236
Yilei Yang <yileiyang@google.com>
2022-04-24 23:24:02 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 444175212

--- jax3d/projects/nesf/nerfstatic/eval_lib.py
+++ jax3d/projects/nesf/nerfstatic/eval_lib.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Evaluation library.
 
 By default, continually evaluates the latest model checkpoints found in
